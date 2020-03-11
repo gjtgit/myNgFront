b30cddb925230a82830c56b9e2dbb251fb267d34
Jian Tuan Gao <gjtgaowh@cn.ibm.com>
2020-03-11 11:19:53 +0800

    change proj name and add test button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { tokenData } from './tokenData';
+import { UserData } from './userData';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +9,38 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myDemo';
   constructor(private _http:HttpClient) {}
-
+  title = 'myFirstNgFront';
+  tokenInfo = 'no token';
+  userData:UserData = new UserData();
   getToken(){
-    this._http.post("http://localhost:8088/oauth/token?grant_type=password&username=gjt&password=123",
-      {headers:new HttpHeaders({'Authorization':'Basic' + btoa('client:secret')})},
-      {withCredentials:true}
+    const getTokenUrl = 'http://localhost:8088/oauth/token';
+    const getTokenParams: HttpParams = new HttpParams()
+            .append('grant_type','password')
+            .append('username',this.userData.username)
+            .append('password',this.userData.password);
+    const getTokenHeaders: HttpHeaders = new HttpHeaders()
+            .append('Authorization','Basic'+btoa('client:secret'));
+    this._http.post<tokenData>(getTokenUrl,
+      {withCredentials:true},
+      {
+        headers:getTokenHeaders,
+        params:getTokenParams
+      }
     ).subscribe(res =>{
       console.log(res);
+    this.tokenInfo = res['access_token'];
       this.getUsernameOfToken(res);
     } );
+   
+    // this._http.post("http://localhost:8088/oauth/token?grant_type=password&username=gjt&password=123",
+    //   {headers:new HttpHeaders({'Authorization':'Basic' + btoa('client:secret')})},
+    //   {withCredentials:true}
+    // ).subscribe(res =>{
+    //   console.log(res);
+    //   this.tokenInfo = res['access_token'];
+    //   this.getUsernameOfToken(res);
+    // } );
 
   }
 
@@ -26,7 +49,7 @@ export class AppComponent {
       {withCredentials: true},
       {headers:new HttpHeaders({'Authorization':'Bearer' + tokenInfo['access_token']})}
     ).subscribe(result =>{
-      // console.log(result);
+       console.log(result);
     } );
 
   }
